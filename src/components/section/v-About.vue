5613a177510be7fd572fb778e011f440729ba797
juniorcesar001 <silvio_cbsj@hotmail.com>
2023-04-29 14:34:08 -0300

    Sobre Mim - Mobile

--- src/components/section/v-About.vue
+++ src/components/section/v-About.vue
@@ -17,37 +17,36 @@ export default {
   <div class="about">
     <div class="about__title">
       <h4><span class="number">01.</span> <span class="title">Sobre mim</span></h4>
-      
-      <div class="about__content">
+    </div>
+    <div class="about__content">
 
-        <div class="text">
-          <p>
-            Atualmente trabalho no e-commerce FutFanatics, estou como Desenvolvedor WEB há +1 ano e meio.
-            Durante esse período, estou aprendendo um leck gigantesco de soft skills como: comunicação 
-            entre as áreas, suporte aos colaboradores, entendimento e mapeamento de processos, e solução de 
-            problemas de forma eficaz e rápida.Tenho um instagram (@junior.coding) com 10K de seguidores, onde 
-            estou compartilhando diversos conhecimentos e dicas para Devs. Pretendo seguir mais forte com as 
-            redes sociais nesse ano de 2023.
-          </p>
+      <div class="text">
+        <p>
+          Atualmente trabalho no e-commerce FutFanatics, estou como Desenvolvedor WEB há +1 ano e meio.
+          Durante esse período, estou aprendendo um leck gigantesco de soft skills como: comunicação 
+          entre as áreas, suporte aos colaboradores, entendimento e mapeamento de processos, e solução de 
+          problemas de forma eficaz e rápida. Tenho um instagram (@junior.coding) com 10K de seguidores, onde 
+          estou compartilhando diversos conhecimentos e dicas para Devs. Pretendo seguir mais forte com as 
+          redes sociais nesse ano de 2023.
+        </p>
 
-          <ul>
-            <li>JavaScript (ES6+)</li>
-            <li>Node.JS</li>
-            <li>React.JS</li>
-            <li>Next.JS</li>
-            <li>Typescript</li>
-            <li>PHP</li>
-            <li>MongoDB</li>
-            <li>MySQL</li>
-          </ul>
-        </div>
-
-        <div class="thumb-border"></div>
-        <div class="thumb">
-          <img :src="thumb.src" :alt="thumb.alt">
-        </div>
-        
+        <ul>
+          <li>JavaScript (ES6+)</li>
+          <li>Node.JS</li>
+          <li>React.JS</li>
+          <li>Next.JS</li>
+          <li>Typescript</li>
+          <li>PHP</li>
+          <li>MongoDB</li>
+          <li>MySQL</li>
+        </ul>
       </div>
+
+      <div class="thumb-border"></div>
+      <div class="thumb">
+        <img :src="thumb.src" :alt="thumb.alt">
+      </div>
+      
     </div>
   </div>
 </template>
